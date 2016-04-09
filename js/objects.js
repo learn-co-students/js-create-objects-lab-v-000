5fb2d35ca64cf55f0a1b91b0eba22119b218c710
c1505 <coreym247@gmail.com>
2016-04-09 19:28:15 -0400

    Done.

--- js/objects.js
+++ js/objects.js
@@ -1,1 +1,48 @@
 'use strict';
+
+function Dog (name, breed, age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Cat (name, breed, age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Ferret (name, age, foodRequirements) {
+  this.name = name;
+  this.age = age;
+  this.foodRequirements = foodRequirements;
+}
+
+function Bird (name, breed) {
+  this.name = name;
+  this.breed = breed;
+}
+
+function Fish (name, species, waterConditions) {
+  this.name = name;
+  this.waterConditions = waterConditions;
+  this.species = species;
+}
+
+var fred = new Dog("fred", "golden", 10);
+var scruffy = new Dog("scruffy", "lab", 5);
+
+var garfield = new Cat("garfield", "calico", 5);
+var pet = new Cat("pet", "nude", 1);
+
+var brad = new Ferret("bard", 100, "none");
+var ferrous = new Ferret("ferrous", 20, "1000 salmon packets");
+
+var birdy = new Bird("polly", "none");
+var birdo = new Bird("parsnip", "toucan");
+
+var fisho = new Fish("fisho", "shark", "salt");
+var fishy = new Fish("fishy", "salmon", "fresh");
+
+
+
