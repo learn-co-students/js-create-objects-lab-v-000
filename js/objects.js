4b8809dd5fb3eb2b68c851dcbf2d7acc23db1f37
Brian <useyourbrian@gmail.com>
2016-04-15 18:09:53 -0400

    Done.

--- js/objects.js
+++ js/objects.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function Dog(name, breed, age){
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Cat(name, breed, age){
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Bird(name, breed){
+  this.name =name;
+  this.breed = breed;
+}
+
+function Fish(name, species, conditions){
+  this.name = name;
+  this.species = species;
+  this.waterConditions = conditions;
+}
+
+function Ferret(name, age, food){
+  this.name = name;
+  this.age = age;
+  this.foodRequirements = food;
+}
