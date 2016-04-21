a84991d4036f3b58b44245580ccaab2c2e7ba97f
Karuna <imkaruna@gmail.com>
2016-04-21 12:03:42 -0400

    Done

--- js/objects.js
+++ js/objects.js
@@ -1,1 +1,33 @@
 'use strict';
+function Dog(dogname, dogbreed, dogage) {
+  this.name = dogname;
+  this.age = dogage;
+  this.breed = dogbreed;
+}
+
+function Cat(catname, catbreed,catage ) {
+  this.name = catname;
+  this.age = catage;
+  this.breed = catbreed;
+
+}
+
+function Ferret(ferretName, ferretAge, ferretFood) {
+  this.name = ferretName;
+  this.age = ferretAge;
+  this.foodRequirements = ferretFood;
+
+}
+
+function Bird(birdName, birdBreed) {
+  this.name = birdName;
+  this.breed = birdBreed;
+
+}
+
+function Fish(fishName, fishSpecies, fishWaterCondition) {
+  this.name = fishName;
+  this.species = fishSpecies;
+  this.waterConditions = fishWaterCondition;
+
+}
