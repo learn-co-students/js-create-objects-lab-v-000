38ba590c308462d3f9ab918adfb2ea8666b7fa66
mirandalily <mirandaraymond@me.com>
2016-06-28 15:17:44 -0700

    Done.

--- js/objects.js
+++ js/objects.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function Dog(name, breed, age) {
+	this.name = name;
+	this.breed = breed;
+	this.age = age;
+}
+
+function Cat(name, breed, age) {
+	this.name = name;
+	this.breed = breed;
+	this.age = age;
+}
+
+function Ferret(name, age, foodRequirements) {
+	this.name = name;
+	this.age = age;
+	this.foodRequirements = foodRequirements;
+}
+
+function Bird(name, breed) {
+	this.name = name;
+	this.breed = breed;
+}
+
+function Fish(name, species, waterConditions) {
+	this.name = name;
+	this.species = species;
+	this.waterConditions = waterConditions;
+}
+
+var marley = new Dog("Marley", "King Charles Cavalier Spaniel", 7);
+var benny = new Cat("Benedict", "Black Cat", 2);
+var strider = new Dog("Strider", 'Mutt', 1);
