26dd930f464d30b701866834797adac5efc3c882
Jason Casares <jasonc@csdrafting.com>
2016-07-31 15:32:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 'use strict';
+
+var new Dog = {
+  name:
+  breed:
+  age:
+}
+
+var Cat = {
+  name:
+  breed:
+  age:
+}
+
+var Ferret = {
+  name:
+  age:
+  foodRequirements:
+}
+
+var Bird = {
+  name:
+  breed:
+}
+
+var Fish = {
+  name:
+  species:
+  waterConditions:
+}
