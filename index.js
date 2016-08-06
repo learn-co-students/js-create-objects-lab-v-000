28c781e71668ed64a2d2a3a9a563c24ec75bacd2
reichardn <reichardn@gmail.com>
2016-08-06 02:42:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function Dog(name, breed, age) {
+  this.name = name; 
+  this.breed = breed;
+  this.age = age;
+}
+
+function Cat(name, breed, age) {
+  this.name = name; 
+  this.breed = breed;
+  this.age = age;
+}
+
+function Ferret(name, age, fr) {
+  this.name = name;
+  this.age = age;
+  this.foodRequirements = fr;
+}
+
+function Bird(name, breed) {
+  this.name = name; 
+  this.breed = breed;
+}
+
+function Fish(name, species, wc) {
+  this.name = name;
+  this.species = species;
+  this.waterConditions = wc;
+}
+
