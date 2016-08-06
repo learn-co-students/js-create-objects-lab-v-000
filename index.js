60da1f721690765cd63e6ed762e5f9aa7b91c230
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-08-06 15:10:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function Dog(name, breed, age) {
+  // body...
+  this.name  =  name;
+  this.breed =  breed;
+  this.age   =  age;
+}
+
+function Cat(name, breed, age) {
+  // body...
+  this.name  =  name;
+  this.breed =  breed;
+  this.age   =  age;
+}
+
+function Ferret(name, age, foodRequirements) {
+  // body...
+  this.name             = name;
+  this.age              = age;
+  this.foodRequirements = foodRequirements;
+}
+
+function Bird(name, breed) {
+  // body...
+  this.name  =  name;
+  this.breed =  breed;
+}
+
+function Fish(name, species, waterConditions) {
+  // body...
+  this.name            =  name;
+  this.species         =  species;
+  this.waterConditions = waterConditions;
+}
