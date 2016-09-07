b76c64c6dcc13cc303df5224eb73203209a74512
azemoh <joshazemoh@gmail.com>
2016-09-07 16:56:39 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-'use strict';
+'use strict'
+
+function Animal(name, breed, age) {
+  this.name = name
+  this.breed = breed
+  this.age = age
+}
+
+function Dog(name, breed, age) {
+  Animal.call(this, name, breed, age)
+}
+
+function Cat(name, breed, age) {
+  Animal.call(this, name, breed, age)
+}
+
+function Ferret(name, age, foodRequirements) {
+  this.name = name
+  this.age = age
+  this.foodRequirements = foodRequirements
+}
+
+function Bird(name, breed) {
+  this.name = name
+  this.breed = breed
+}
+
+function Fish(name, species, waterConditions) {
+  this.name = name
+  this.species = species
+  this.waterConditions = waterConditions
+}
