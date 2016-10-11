20c91ce45eaccb97f1994bcd3252d809d408237d
rgylling <mikegylling@yahoo.com>
2016-10-11 11:47:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function Dog(name,breed,age){
+  this.name = name;
+  this.age = age;
+  this.breed = breed;
+}
+
+function Cat(name,breed,age){
+  this.name = name;
+  this.age = age;
+  this.breed = breed;
+}
+
+function Ferret(name,age,food){
+  this.name = name;
+  this.age = age;
+  this.foodRequirements = food;
+}
+
+function Bird(name,breed){
+  this.name = name;
+  this.breed = breed;
+}
+
+function Fish(name,species,water){
+  this.name = name;
+  this.species = species;
+  this.waterConditions = water;
+}
