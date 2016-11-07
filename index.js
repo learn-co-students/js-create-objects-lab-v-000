990ccf7160455c1455f543d4fb0253a5974a0947
Han Lee <ralphgoodwin@Ralphs-MacBook-Air.local>
2016-11-07 11:04:08 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function Dog (name, breed, age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Cat (name, breed, age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Ferret (name, age, foodRequirements) {
+  this.name = name;
+  this.foodRequirements = foodRequirements;
+  this.age = age;
+}
+
+function Bird (name, breed) {
+  this.name = name;
+  this.breed = breed;
+}
+
+function Fish (name, species, waterConditions) {
+  this.name = name;
+  this.species = species;
+  this.waterConditions = waterConditions;
+}
+
+
+
+    // Dog with name, breed, and age properties
+    // Cat with name, breed, and age properties
+    // Ferret with name, age, and foodRequirements properties
+    // Bird with name and breed properties
+    // Fish with name, species, and waterConditions properties
