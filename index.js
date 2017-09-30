3195119e1922339cf8a7b961b5b52ce23d48ed02
Matteo Piccini <gnappo.1@live.it>
2017-09-30 00:37:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 'use strict';
+function Dog(name, breed, age) {
+  this.name = name;
+  this.age = age;
+  this.breed = breed;
+}
+
+function Cat(name, breed, age) {
+  this.name = name;
+  this.age = age;
+  this.breed = breed;
+}
+
+function Ferret(name, age, foodRequirements) {
+  this.name = name;
+  this.foodRequirements = foodRequirements;
+  this.age = age;
+}
+
+function Bird(name, breed) {
+  this.name = name;
+  this.breed = breed;
+}
+
+function Fish(name, species, waterConditions) {
+  this.name = name;
+  this.waterConditions = waterConditions;
+  this.species = species;
+}
