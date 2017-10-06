45d6db82c8d0da461962c851e893b75a4c02e04f
James Fernandez <jffernan@comcast.net>
2017-10-06 14:42:34 -0700

    Re-do & added pseudocode // comments

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-'use strict';
-
+/* 
+Create a constructor function for each of the following:
+1. Dog with name, breed, and age properties
+2. Cat with name, breed, and age properties
+3. Ferret with name, age, and foodRequirements properties
+4. Bird with name and breed properties
+5. Fish with name, species, and waterConditions properties
+*/
 function Dog(name, breed, age){
   this.name = name
   this.breed = breed
